5157f11cfb6f6c0e95707bceef77e4e936564a2a
Sharmitha G <sharmithapriyanka@gmail.com>
2026-07-09 12:23:28 +0530

    Create 766-ToeplitzMatrix

---
+++ 766-ToeplitzMatrix/version/java/766-ToeplitzMatrix_20260709_122327.java
@@ -0,0 +1,16 @@
+// Last updated: 7/9/2026, 12:23:27 PM
+1class Solution {
+2    public boolean isToeplitzMatrix(int[][] matrix) {
+3        int m = matrix.length, n = matrix[0].length;
+4
+5        for (int i = 1; i < m; i++) {
+6            for (int j = 1; j < n; j++) {
+7                if (matrix[i][j] != matrix[i - 1][j - 1]) {
+8                    return false;
+9                }
+10            }
+11        }
+12
+13        return true;
+14    }
+15}
