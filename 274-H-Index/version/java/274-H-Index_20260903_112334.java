f97b7bf10600bf68af74349cf6166859e39bfe95
Sharmitha G <sharmithapriyanka@gmail.com>
2026-09-03 11:23:36 +0530

    Create 274-H-Index

---
+++ 274-H-Index/version/java/274-H-Index_20260903_112334.java
@@ -0,0 +1,20 @@
+// Last updated: 9/3/2026, 11:23:34 AM
+1import java.util.Arrays;
+2
+3class Solution {
+4    public int hIndex(int[] citations) {
+5        Arrays.sort(citations);
+6
+7        int n = citations.length;
+8
+9        for (int i = 0; i < n; i++) {
+10            int h = n - i;
+11
+12            if (citations[i] >= h) {
+13                return h;
+14            }
+15        }
+16
+17        return 0;
+18    }
+19}
